33717939901cb7de3efa03f6357b53a465521dbb
Dhairya <dhairya15marwaha@gmail.com>
2026-07-11 12:11:48 +0530

    fix: restore unread notification count

--- gameplan/gameplan/doctype/gp_notification/test_gp_notification.py
+++ gameplan/gameplan/doctype/gp_notification/test_gp_notification.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2022, Frappe Technologies Pvt Ltd and Contributors
 # See license.txt
 
-# import frappe
+from unittest.mock import patch
+
+import frappe
 from frappe.tests.utils import FrappeTestCase
+
+from gameplan.api import unread_notifications
 
 
 class TestGPNotification(FrappeTestCase):
-	pass
+	def test_unread_notifications_counts_for_current_user(self):
+		with patch.object(frappe.db, "count", return_value=3) as count:
+			self.assertEqual(unread_notifications(), 3)
+
+		count.assert_called_once_with(
+			"GP Notification",
+			filters={"to_user": frappe.session.user, "read": 0},
+		)
